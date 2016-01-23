ac81ffd3423e46e650d06d6244363e12f397a8b7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-01-23 12:14:30 +0100

    New attempt at a travis file

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -5,7 +5,8 @@ import           Test.QuickCheck
 
 import           Control.Monad         (forM, forM_)
 
-import           System.Directory      (getDirectoryContents)
+import           System.Directory      (doesDirectoryExist,
+                                        getDirectoryContents)
 import           System.FilePath.Posix ((<.>), (</>))
 
 concerningContents :: (FilePath -> String -> SpecWith a) -> (FilePath -> SpecWith a)
@@ -14,9 +15,13 @@ concerningContents func file = runIO (readFile file) >>= func file
 forFileInDirss :: [FilePath] -> (FilePath -> SpecWith a) -> SpecWith a
 forFileInDirss dirs func = do
     allFiles <- fmap concat $ forM dirs $ \dir -> do
-        files <- runIO (getDirectoryContents dir)
-        let ffiles = filter (`notElem` [".", ".."]) files
-        return $ map (dir </>) ffiles
+        exists <- runIO $ doesDirectoryExist dir
+        if exists
+        then do
+            files <- runIO $ getDirectoryContents dir
+            let ffiles = filter (`notElem` [".", ".."]) files
+            return $ map (dir </>) ffiles
+        else return []
     forM_ allFiles func
 
 pend = it "has no tests yet" pending
