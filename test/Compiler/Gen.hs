95c0c8cd31745040d2059aa016f96d8ea5bfa19b
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-02-03 21:13:25 +0100

    Started on compiler arbitrary instances

---
+++ test/Compiler/Gen.hs
@@ -0,0 +1,9 @@
+module Compiler.Gen (
+      module Compiler.Gen
+    , module Parser.Gen
+    ) where
+
+-- import           Test.Hspec
+-- import           Test.QuickCheck
+
+import           Parser.Gen
