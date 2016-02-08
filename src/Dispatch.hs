c15427771a377c062f7bd6b930f8d3d954b484cf
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-02-08 21:07:19 +0100

    Applied an untested seeding stage, it's functional again

--- src/Dispatch.hs
+++ src/Dispatch.hs
@@ -1,30 +1,41 @@
-module Dispatch where
+module Dispatch (dispatch) where
 
 import           Compiler
+import           Compiler.Types
+import           Control.Monad  (void)
 import           Deployer
 import           Deployer.Types
 import           Dispatch.Types
 import           Formatter
 import           Parser
+import           Seed
 import           Types
 
 dispatch :: Dispatch -> Sparker ()
-dispatch (DispatchParse fp) = parseFile fp >> return () -- Just parse, throw away the results.
+dispatch (DispatchParse fp) = do
+    void $ parseFile fp  -- Just parse, throw away the results.
+
 dispatch (DispatchFormat fp) = do
     sf <- parseFile fp
     str <- formatSparkFile sf
     liftIO $ putStrLn str
+
 dispatch (DispatchCompile cfr) = do
     deployments <- compileJob cfr
     outputCompiled deployments
+
 dispatch (DispatchCheck ccr) = do
-    deps <- case ccr of
-        DeployerCardCompiled fp -> inputCompiled fp
-        DeployerCardUncompiled cfr -> compileJob cfr
-    pdps <- check deps
-    liftIO $ putStr $ formatPreDeployments $ zip deps pdps
+    deps <- deployCompiledCardRef ccr
+    seeded <- seedByCompiledCardRef ccr deps
+    pdps <- check seeded
+    liftIO $ putStr $ formatPreDeployments $ zip seeded pdps
+
 dispatch (DispatchDeploy dcr) = do
-    deps <- case dcr of
-        DeployerCardCompiled fp -> inputCompiled fp
-        DeployerCardUncompiled cfr -> compileJob cfr
-    deploy deps
+    deps <- deployCompiledCardRef dcr
+    seeded <- seedByCompiledCardRef dcr deps
+    deploy seeded
+
+
+deployCompiledCardRef :: DeployerCardReference -> Sparker [Deployment]
+deployCompiledCardRef (DeployerCardCompiled fp) = inputCompiled fp
+deployCompiledCardRef (DeployerCardUncompiled cfr) = compileJob cfr
