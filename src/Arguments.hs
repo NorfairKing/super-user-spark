2a2ca0fdef0acbe48b49cde191c42e1949bb833e
stites <sam@stites.io>
2017-11-02 12:56:35 -0400

    use optparse_applicative macro for version

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -50,7 +50,7 @@ runOptionsParser strs = execParserPure prefs optionsParser strs
       { prefMultiSuffix = "SPARK"  -- metavar suffix for multiple options
       , prefDisambiguate = True    -- automatically disambiguate abbreviations (default: False)
       , prefShowHelpOnError = True -- always show help text on parse errors (default: False)
-#if MIN_VERSION_base(4,9,1)
+#if MIN_VERSION_optparse_applicative(0,13,0)
       , prefShowHelpOnEmpty = True -- always show help text on empty values (default: False)
 #endif
       , prefBacktrack = True       -- backtrack to parent parser when a subcommand fails (default: True)
